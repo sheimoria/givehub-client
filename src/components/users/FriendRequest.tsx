dd337c77ec4f1aba171de8edfaf1b7b8cd9c7da1
Shem Maleriado <shem.maleriado@gmail.com>
2021-08-16 12:49:29 +0800

    fix: friend and volunteer request accept and reject buttons

--- src/components/users/FriendRequest.tsx
+++ src/components/users/FriendRequest.tsx
@@ -4,53 +4,54 @@ import {
   useAcceptFriendRequestMutation
 } from 'generated/graphql'
 
-import Link from 'next/link'
 import Picture from 'components/Picture'
+import router from 'next/router'
+import { CheckCircleIcon, XCircleIcon } from '@heroicons/react/solid'
+import React from 'react'
 
 export default function FriendRequest({ user }: { user: UserHeaderFragment }) {
   const [acceptFriendRequest] = useAcceptFriendRequestMutation()
 
   return (
     <div className="flex flex-wrap justify-between gap-3">
-      <div className="flex items-center gap-3">
+      <div
+        onClick={() =>
+          router.push({
+            pathname: '/user',
+            query: { userId: user.id }
+          })
+        }
+        className="flex items-center gap-4"
+      >
         <Picture pictureId={user.profile?.displayPicture} />
         <div className="flex flex-col">
-          <Link
-            href={{
-              pathname: '/user',
-              query: { userId: user.id }
-            }}
-          >
-            <a className="truncate">
-              {user.profile?.firstName} {user.profile?.lastName}
-            </a>
-          </Link>
-          <p className="text-xs truncate">@{user.username}</p>
+          <h6 className="truncate transition-colors hover:text-gray-800 dark:hover:text-gray-100">
+            {user.profile?.firstName} {user.profile?.lastName}
+          </h6>
+          <span className="text-xs truncate text-rose-600">
+            @{user.username}
+          </span>
         </div>
       </div>
       <div className="flex items-center flex-none gap-2">
-        <button
+        <XCircleIcon
           onClick={() =>
             acceptFriendRequest({
               variables: { userId: user.id, accept: false },
               refetchQueries: [{ query: FriendRequestsDocument }]
             })
           }
-          className="px-3 py-1 button-outline"
-        >
-          Reject
-        </button>
-        <button
+          className="w-7 h-7 clickable"
+        />
+        <CheckCircleIcon
           onClick={() =>
             acceptFriendRequest({
               variables: { userId: user.id, accept: true },
               refetchQueries: [{ query: FriendRequestsDocument }]
             })
           }
-          className="px-3 py-1"
-        >
-          Accept
-        </button>
+          className="transition-colors cursor-pointer w-7 h-7 text-rose-600 hover:text-rose-700 dark:text-rose-500"
+        />
       </div>
     </div>
   )
