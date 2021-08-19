11cbb9b3452f90e723d5606ed6e31a9b5dc14a79
Shem Maleriado <shem.maleriado@gmail.com>
2021-08-20 02:01:01 +0800

    refactor: user post card styling

--- src/components/users/UserPost.tsx
+++ src/components/users/UserPost.tsx
@@ -11,12 +11,12 @@ export default function UserPost({ user }: { user: UserPictureFragment }) {
     <>
       <Transit as="article" className="flex-row items-center px-6 pb-6">
         <Picture pictureId={user.profile?.displayPicture} />
-        <span
+        <button
           onClick={toggleIsOpen}
-          className="flex flex-auto text-sm text-gray-500 cursor-pointer hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
+          className="flex flex-1 px-4 py-2 text-sm text-gray-500 transition-colors bg-gray-100 rounded-md dark:hover:bg-gray-600 hover:bg-gray-200 dark:bg-gray-700 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-300"
         >
-          New Post &hellip;
-        </span>
+          New Post
+        </button>
       </Transit>
       {isOpen && <CreatePostModal toggleIsOpen={toggleIsOpen} />}
     </>
