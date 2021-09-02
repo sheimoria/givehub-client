a00267799f2cad859205526fd3ef469a806423d5
Shem Maleriado <shem.maleriado@gmail.com>
2021-09-03 01:53:22 +0800

    feat: add yarn

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import 'styles/globals.css'
 
 import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
-import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
 import client from 'utils/apollo-client'
 
